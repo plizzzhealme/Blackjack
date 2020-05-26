2a545b03c0be45781f3c57f699b2f056320c9325
Den <plizzz.healme@gmail.com>
2020-05-26 14:45:23 +0300

    replace array with array list

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,45 +1,34 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
     private int current;
-    private final Card[] cards;
+    private final List<Card> cards;
 
     public Deck() {
-        cards = new BlackjackCard[52];
-        int i = 0;
+        cards = new ArrayList<>(52);
 
         for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
             for (int rank = Card.ACE; rank <= Card.KING; rank++) {
-                cards[i] = new BlackjackCard(rank, suit);
-                i++;
+                cards.add(new BlackjackCard(rank, suit));
             }
         }
-
     }
 
     public Card getNextCard() {
-        Card card = cards[current];
-        current++;
-        return card;
+        return cards.get(current++);
     }
 
     public void shuffle() {
         current = 0;
-        Random rnd = new Random();
-
-        for (int i = 1; i < cards.length; i++) {
-            int j = rnd.nextInt(i);
-            Card temp = cards[i];
-            cards[i] = cards[j];
-            cards[j] = temp;
-        }
+        Collections.shuffle(cards);
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(cards);
+        return cards.toString();
     }
 }
